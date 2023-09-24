38c10c47c6f12282eba5b919c0e511db8d3674c6
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-09-24 11:02:18 +0900

    format

--- frontend/src/IssueList.tsx
+++ frontend/src/IssueList.tsx
@@ -249,75 +249,84 @@ function IssueList() {
             {issues.map((issue, index) => (
               <TableRow key={issue.id}>
                 <TableCell>
-                <Box display="flex" flexDirection="column">
-    <Link
-      href={issue.html_url}
-      target="_blank"
-      rel="noopener noreferrer"
-      underline="none"
-      color="inherit"
-    >
-      <Box display="flex" alignItems="center" gap={1}>
-        <Typography variant="body1">{issue.title}</Typography>
-        {issue.labels &&
-          issue.labels.map((label) => (
-            <Chip
-              key={label.id}
-              label={label.name}
-              sx={{
-                backgroundColor: `#${label.color}`,
-                color: '#000000',
-              }}
-              size="small"
-            />
-          ))}
-      </Box>
-    </Link>
-    <Typography
-      variant="body2"
-      color="textSecondary"
-      sx={{ mt: 1 }}
-    >
-      #{issue.number} opened {issue.created_at.toString()} by {issue.user.login}
-    </Typography>
-  </Box>
-</TableCell>
+                  <Box display="flex" flexDirection="column">
+                    <Link
+                      href={issue.html_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      underline="none"
+                      color="inherit"
+                    >
+                      <Box display="flex" alignItems="center" gap={1}>
+                        <Typography variant="body1">{issue.title}</Typography>
+                        {issue.labels &&
+                          issue.labels.map((label) => (
+                            <Chip
+                              key={label.id}
+                              label={label.name}
+                              sx={{
+                                backgroundColor: `#${label.color}`,
+                                color: '#000000',
+                              }}
+                              size="small"
+                            />
+                          ))}
+                      </Box>
+                    </Link>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      sx={{ mt: 1 }}
+                    >
+                      #{issue.number} opened {issue.created_at.toString()} by{' '}
+                      {issue.user.login}
+                    </Typography>
+                  </Box>
+                </TableCell>
 
                 <Hidden smDown>
-                <TableCell>
-                <Box display="flex" justifyContent="center" alignItems="center">
-                    {issue.assignees &&
-                      issue.assignees.map((assignee) => (
-                        <Box
-                          key={assignee.id}
-                          display="flex"
-                          alignItems="center"
-                          sx={{ mr: 2 }}
-                        >
-                          <Avatar
-                            src={assignee.avatar_url}
-                            alt={assignee.login}
-                            sx={{ width: 24, height: 24, mr: 0.5 }}
-                          />
+                  <TableCell>
+                    <Box
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      {issue.assignees &&
+                        issue.assignees.map((assignee) => (
+                          <Box
+                            key={assignee.id}
+                            display="flex"
+                            alignItems="center"
+                            sx={{ mr: 2 }}
+                          >
+                            <Avatar
+                              src={assignee.avatar_url}
+                              alt={assignee.login}
+                              sx={{ width: 24, height: 24, mr: 0.5 }}
+                            />
+                            <Typography variant="body2">
+                              {assignee.login}
+                            </Typography>
+                          </Box>
+                        ))}
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    <Box
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      {issue.comments > 0 && (
+                        <>
+                          <CommentIcon sx={{ mr: 1 }} />
                           <Typography variant="body2">
-                            {assignee.login}
+                            {issue.comments}
                           </Typography>
-                        </Box>
-                      ))}
-                  </Box>
-                </TableCell>
-                <TableCell>
-                  <Box display="flex" justifyContent="center" alignItems="center">
-                    {issue.comments > 0 && (
-                      <>
-                        <CommentIcon sx={{ mr: 1 }} />
-                        <Typography variant="body2">
-                          {issue.comments}
-                        </Typography>
-                      </>
-                    )}
-                  </Box>
-                </TableCell>
+                        </>
+                      )}
+                    </Box>
+                  </TableCell>
                 </Hidden>
               </TableRow>
             ))}
