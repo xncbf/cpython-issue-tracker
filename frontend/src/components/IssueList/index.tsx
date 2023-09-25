425a4a5d00fd2c7564cf9e7ce0e7a65f852e9fd1
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-09-25 22:04:17 +0900

    clean

--- frontend/src/components/IssueList/index.tsx
+++ frontend/src/components/IssueList/index.tsx
@@ -31,6 +31,192 @@ import SearchField from './SearchField';
 import LabelFilter from './LabelFilter';
 import useInfiniteScroll from './useInfiniteScroll';
 
+type FiltersProps = {
+  searchFilter: string;
+  labelFilter: string[];
+  labels: Label[];
+  issueStatus: 'ALL' | 'OPEN' | 'CLOSED';
+  issueFilter: string;
+  handleFilterChange: (event: any, key?: any) => void;
+};
+
+function Filters({
+  searchFilter,
+  labelFilter,
+  labels,
+  issueStatus,
+  issueFilter,
+  handleFilterChange,
+}: FiltersProps) {
+  return (
+    <Box sx={{ marginBottom: 3 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          mb: 3,
+          justifyContent: 'space-between',
+          gap: 2,
+        }}
+      >
+        <SearchField value={searchFilter} onChange={handleFilterChange} />
+        <FormControl variant="outlined" sx={{ flex: 1 }}>
+          <LabelFilter
+            labels={labels}
+            value={labelFilter}
+            onChange={handleFilterChange}
+          />
+        </FormControl>
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          mb: 3,
+          justifyContent: 'space-between',
+          gap: 2,
+        }}
+      >
+        <FormControl variant="outlined" sx={{ flex: 1 }}>
+          <InputLabel id="issue-status-label">status</InputLabel>
+          <Select
+            labelId="issue-status-label"
+            id="issue-status"
+            value={issueStatus}
+            label="status"
+            name="status"
+            onChange={handleFilterChange}
+          >
+            <MenuItem value="ALL">All</MenuItem>
+            <MenuItem value="OPEN">Open</MenuItem>
+            <MenuItem value="CLOSED">Closed</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl variant="outlined" sx={{ flex: 1 }}>
+          <InputLabel id="issue-type-filter-label">Type</InputLabel>
+          <Select
+            labelId="issue-type-filter-label"
+            id="issue-type-filter"
+            value={issueFilter}
+            label="issue-type"
+            onChange={handleFilterChange}
+            name="is_issue"
+          >
+            <MenuItem value="ALL">All</MenuItem>
+            <MenuItem value="ISSUE">Issue</MenuItem>
+            <MenuItem value="PULL REQUEST">Pull Request</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </Box>
+  );
+}
+
+type IssueRowProps = {
+  issue: Issue;
+};
+
+function IssueRow({ issue }: IssueRowProps) {
+  return (
+    <TableRow key={issue.id}>
+      <TableCell>
+        <Box display="flex" flexDirection="column">
+          <Link
+            href={issue.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            underline="none"
+            color="inherit"
+          >
+            <Box display="flex" alignItems="center" gap={1}>
+              <Typography variant="body1">{issue.title}</Typography>
+              {issue.labels &&
+                issue.labels.map((label) => (
+                  <Chip
+                    key={label.id}
+                    label={label.name}
+                    sx={{
+                      backgroundColor: `#${label.color}`,
+                      color: '#000000',
+                    }}
+                    size="small"
+                  />
+                ))}
+            </Box>
+          </Link>
+          <Typography variant="body2" color="textSecondary" sx={{ mt: 1 }}>
+            #{issue.number} opened {issue.created_at.toString()} by{' '}
+            {issue.user.login}
+          </Typography>
+        </Box>
+      </TableCell>
+
+      <Hidden smDown>
+        <TableCell>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            {issue.assignees &&
+              issue.assignees.map((assignee) => (
+                <Box
+                  key={assignee.id}
+                  display="flex"
+                  alignItems="center"
+                  sx={{ mr: 2 }}
+                >
+                  <Avatar
+                    src={assignee.avatar_url}
+                    alt={assignee.login}
+                    sx={{ width: 24, height: 24, mr: 0.5 }}
+                  />
+                  <Typography variant="body2">{assignee.login}</Typography>
+                </Box>
+              ))}
+          </Box>
+        </TableCell>
+        <TableCell>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            {issue.comments > 0 && (
+              <>
+                <CommentIcon sx={{ mr: 1 }} />
+                <Typography variant="body2">{issue.comments}</Typography>
+              </>
+            )}
+          </Box>
+        </TableCell>
+      </Hidden>
+    </TableRow>
+  );
+}
+
+type IssuesTableProps = {
+  issues: Issue[];
+};
+
+function IssuesTable({ issues }: IssuesTableProps) {
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell style={{ fontWeight: 'bold' }}>Issue</TableCell>
+          <Hidden smDown>
+            <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>
+              Assignees
+            </TableCell>
+            <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>
+              Comments
+            </TableCell>
+          </Hidden>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {issues.map((issue) => (
+          <IssueRow key={issue.id} issue={issue} />
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
 function IssueList() {
   const [issues, setIssues] = useState<Issue[]>([]);
   const [labels, setLabels] = useState<Label[]>([]);
@@ -98,13 +284,7 @@ function IssueList() {
   }, [fetchData]);
   useInfiniteScroll(throttledFetchData);
 
-  type StandardEvent = {
-    target: {
-      name: string;
-      value: any;
-    };
-  };
-  const handleFilterChange = (event: StandardEvent | string, key?: any) => {
+  const handleFilterChange = (event: any | string, key?: any) => {
     let name: string;
     let value: any;
 
@@ -143,168 +323,15 @@ function IssueList() {
         <Typography variant="h4" gutterBottom>
           CPython Issues
         </Typography>
-
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            mb: 3,
-            justifyContent: 'space-between',
-            gap: 2,
-          }}
-        >
-          <SearchField value={searchFilter} onChange={handleFilterChange} />
-          <FormControl variant="outlined" sx={{ flex: 1 }}>
-            <LabelFilter
-              labels={labels}
-              value={labelFilter}
-              onChange={handleFilterChange}
-            />
-          </FormControl>
-        </Box>
-
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            mb: 3,
-            justifyContent: 'space-between',
-            gap: 2,
-          }}
-        >
-          <FormControl variant="outlined" sx={{ flex: 1 }}>
-            <InputLabel id="issue-status-label">status</InputLabel>
-            <Select
-              labelId="issue-status-label"
-              id="issue-status"
-              value={issueStatus}
-              label="status"
-              name="status"
-              onChange={handleFilterChange}
-            >
-              <MenuItem value="ALL">All</MenuItem>
-              <MenuItem value="OPEN">Open</MenuItem>
-              <MenuItem value="CLOSED">Closed</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl variant="outlined" sx={{ flex: 1 }}>
-            <InputLabel id="issue-type-filter-label">Type</InputLabel>
-            <Select
-              labelId="issue-type-filter-label"
-              id="issue-type-filter"
-              value={issueFilter}
-              label="issue-type"
-              onChange={handleFilterChange}
-              name="is_issue"
-            >
-              <MenuItem value="ALL">All</MenuItem>
-              <MenuItem value="ISSUE">Issue</MenuItem>
-              <MenuItem value="PULL REQUEST">Pull Request</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ fontWeight: 'bold' }}>Issue</TableCell>
-              <Hidden smDown>
-                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                  Assignees
-                </TableCell>
-                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                  Comments
-                </TableCell>
-              </Hidden>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {issues.map((issue, index) => (
-              <TableRow key={issue.id}>
-                <TableCell>
-                  <Box display="flex" flexDirection="column">
-                    <Link
-                      href={issue.html_url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      underline="none"
-                      color="inherit"
-                    >
-                      <Box display="flex" alignItems="center" gap={1}>
-                        <Typography variant="body1">{issue.title}</Typography>
-                        {issue.labels &&
-                          issue.labels.map((label) => (
-                            <Chip
-                              key={label.id}
-                              label={label.name}
-                              sx={{
-                                backgroundColor: `#${label.color}`,
-                                color: '#000000',
-                              }}
-                              size="small"
-                            />
-                          ))}
-                      </Box>
-                    </Link>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      sx={{ mt: 1 }}
-                    >
-                      #{issue.number} opened {issue.created_at.toString()} by{' '}
-                      {issue.user.login}
-                    </Typography>
-                  </Box>
-                </TableCell>
-
-                <Hidden smDown>
-                  <TableCell>
-                    <Box
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      {issue.assignees &&
-                        issue.assignees.map((assignee) => (
-                          <Box
-                            key={assignee.id}
-                            display="flex"
-                            alignItems="center"
-                            sx={{ mr: 2 }}
-                          >
-                            <Avatar
-                              src={assignee.avatar_url}
-                              alt={assignee.login}
-                              sx={{ width: 24, height: 24, mr: 0.5 }}
-                            />
-                            <Typography variant="body2">
-                              {assignee.login}
-                            </Typography>
-                          </Box>
-                        ))}
-                    </Box>
-                  </TableCell>
-                  <TableCell>
-                    <Box
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      {issue.comments > 0 && (
-                        <>
-                          <CommentIcon sx={{ mr: 1 }} />
-                          <Typography variant="body2">
-                            {issue.comments}
-                          </Typography>
-                        </>
-                      )}
-                    </Box>
-                  </TableCell>
-                </Hidden>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <Filters
+          searchFilter={searchFilter}
+          handleFilterChange={handleFilterChange}
+          labelFilter={labelFilter}
+          labels={labels}
+          issueStatus={issueStatus}
+          issueFilter={issueFilter}
+        />
+        <IssuesTable issues={issues} />
       </Box>
     </Container>
   );
