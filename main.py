d1478842bb84d6a779ca8d3522a0c43198e71089
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-09-25 21:08:28 +0900

    gather issue faster

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 import django
@@ -13,50 +14,66 @@ HEADERS = {
     "Authorization": f"Bearer {os.environ['GITHUB_TOKEN']}",
 }
 
+MAX_CONCURRENT_REQUESTS = 10
 
-def fetch_all_issues():
+
+async def fetch_issues_for_page(page, session):
+    response = await session.get(
+        GITHUB_API_URL + "?filter=all&state=all",
+        headers=HEADERS,
+        params={"page": page, "per_page": 100},
+    )
+    if response.status_code != 200:
+        print(response.status_code, response.text)
+        return []
+
+    return response.json()
+
+
+async def fetch_all_issues_async():
     page = 1
-    with httpx.Client() as client:
+
+    async with httpx.AsyncClient() as client:
         while True:
-            response = client.get(
-                GITHUB_API_URL + "?filter=all&state=all",
-                headers=HEADERS,
-                params={"page": page, "per_page": 100},
-            )
-            if response.status_code != 200:
-                print(response.status_code, response.text)
+            tasks = []
+            for _ in range(MAX_CONCURRENT_REQUESTS):
+                tasks.append(fetch_issues_for_page(page, client))
+                page += 1
+
+            pages_data = await asyncio.gather(*tasks)
+            if all(not data for data in pages_data):
                 break
 
-            issues = response.json()
-            if not issues:
-                break
+            for issues in pages_data:
+                if not issues:
+                    continue
+                print(page)
+                for issue_data in issues:
+                    labels = issue_data.pop("labels")
+                    assignee = issue_data.pop("assignee", None)
+                    if assignee:
+                        user, _ = await User.objects.aget_or_create(id=assignee.pop("id"), defaults=assignee)
+                        issue_data["assignee"] = user
+                    assignees = issue_data.pop("assignees", None)
+                    pull_request = issue_data.pop("pull_request", None)
+                    if pull_request:
+                        issue_data["is_issue"] = False
+                    user = issue_data.pop("user")
+                    user, _ = await User.objects.aget_or_create(id=user.pop("id"), defaults=user)
+                    issue_data["user"] = user
 
-            print(page)
-            for issue_data in issues:
-                labels = issue_data.pop("labels")
-                assignee = issue_data.pop("assignee", None)
-                if assignee:
-                    user, _ = User.objects.get_or_create(id=assignee.pop("id"), defaults=assignee)
-                    issue_data["assignee"] = user
-                assignees = issue_data.pop("assignees", None)
-                pull_request = issue_data.pop("pull_request", None)
-                if pull_request:
-                    issue_data["is_issue"] = False
-                user = issue_data.pop("user")
-                user, _ = User.objects.get_or_create(id=user.pop("id"), defaults=user)
-                issue_data["user"] = user
+                    issue, _ = await Issue.objects.aget_or_create(id=issue_data.pop("id"), defaults=issue_data)
+                    if assignees:
+                        assignees_set = set()
+                        for assignee in assignees:
+                            user, _ = await User.objects.aget_or_create(id=assignee.pop("id"), defaults=assignee)
+                            assignees_set.add(user)
 
-                issue, _ = Issue.objects.get_or_create(id=issue_data.pop("id"), defaults=issue_data)
-                if assignees:
-                    issue.assignees.set(
-                        [
-                            User.objects.get_or_create(id=assignee.pop("id"), defaults=assignee)[0]
-                            for assignee in assignees
-                        ]
-                    )
-                labels = [Label.objects.get(id=label["id"]) for label in labels]
-                issue.labels.set(labels)
-            page += 1
+                        await issue.assignees.aset(assignees_set)
+
+                    labels = [await Label.objects.aget(id=label["id"]) for label in labels]
+                    await issue.labels.aset(labels)
+                page += 1
 
 
 def fetch_all_labels():
@@ -77,4 +94,4 @@ def fetch_all_labels():
 
 if __name__ == "__main__":
     fetch_all_labels()
-    fetch_all_issues()
+    asyncio.run(fetch_all_issues_async())
